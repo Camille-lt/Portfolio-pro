9693483edb3cb09e5647963f96bb153d86d12ed9
Camille-lt <camillelebigot5@gmail.com>
2025-06-22 15:12:22 +0200

    add color background changing and new skills cards and update css

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ githubBtn.addEventListener('click', (e) => {
     e.preventDefault();
     window.open('https://github.com/Camille-lt', '_blank');
 });
+
+const cards = document.querySelectorAll('.Skills-card');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+cards.forEach(card => {
+  observer.observe(card);
+});
