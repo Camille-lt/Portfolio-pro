cf765698646a30cc33c604b0e896668296de04d9
Camille-lt <camillelebigot5@gmail.com>
2026-01-27 10:49:03 +0100

    updated CV

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const linkedinBtn = document.querySelector('.linkedin-btn');
 const githubBtn = document.querySelector('.github-btn');
 const linkedinBtnOne = document.querySelector('.linkedin-btn-one');
 const githubBtnOne = document.querySelector('.github-btn-one');
+const links = document.querySelectorAll('.nav-items a');
 
 
+
+links.forEach(link => {
+    link.addEventListener('click', function() {
+        links.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
 
 linkedinBtnOne.addEventListener('click', (e) => {
     e.preventDefault();
